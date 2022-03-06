9f80df72e59260a9326dae62ad300c29299a9709
Damanpreet Singh <daman0797@gmail.com>
2022-03-05 22:17:22 -0500

    Initial Commit & Crypto exchange

---
+++ src/components/CurrencyConverter.jsx
@@ -0,0 +1,103 @@
+import ExchangeRate from './ExchangeRate';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const CurrencyConverter = () => {
+    const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA']
+
+    const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC')
+    const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC')
+    const [amount, setAmount] = useState(1)
+    const [exchangeRate, setExchangeRate] = useState(0)
+    const [result, setResult] = useState(0)
+
+    // console.log(chosenPrimaryCurrency + " primary")
+    // console.log(chosenSecondaryCurrency + " secondary")
+    // console.log(amount + " amount")
+
+    const convert = () => {
+        const options = {
+        method: 'GET',
+        url: 'https://alpha-vantage.p.rapidapi.com/query',
+        params: {from_currency: 'BTC', function: 'CURRENCY_EXCHANGE_RATE', to_currency: 'USD'},
+        headers: {
+            'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+            'x-rapidapi-key': '4d0ce39ae0mshc598194422c08fcp14507cjsn251f20969c99'
+        }
+        };
+
+        axios.request(options).then((response) => {
+            console.log(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+            setExchangeRate(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+            setResult(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] * amount)
+        }).catch((error) => {
+            console.error(error)
+        });
+    }
+
+    // console.log(exchangeRate)
+
+    return (
+        <div className='currency-converter'>
+            <h2>Currency Converter</h2>
+
+            <div className='input-box'>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Primary Currency:</td>
+                            <td>
+                                <input 
+                                    type="number" 
+                                    name='currency-amount-1'
+                                    value={amount}
+                                    onChange={(e) => setAmount(e.target.value)}
+                                />
+                            </td>
+                            <td>
+                                <select
+                                    value={chosenPrimaryCurrency}
+                                    name='currency-option-1'
+                                    className='currency-options'
+                                    onChange={(e) => setChosenPrimaryCurrency(e.target.value)}
+                                >
+                                    {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Secondary Currency:</td>
+                            <td>
+                                <input 
+                                    name='currency-amount-2'
+                                    value={result}
+                                    disabled={true}
+                                />
+                            </td>
+                            <td>
+                                <select
+                                    value={chosenSecondaryCurrency}
+                                    name='currency-option-2'
+                                    className='currency-options'
+                                    onChange={(e) => setChosenSecondaryCurrency(e.target.value)}
+                                >
+                                    {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
+                                </select>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <button id='convert-button' onClick={convert}>Convert</button>
+            </div>
+
+            <ExchangeRate 
+                exchangeRate={exchangeRate}
+                chosenPrimaryCurrency={chosenPrimaryCurrency}
+                chosenSecondaryCurrency={chosenSecondaryCurrency}
+            />
+        </div>
+    );
+}
+
+export default CurrencyConverter;
